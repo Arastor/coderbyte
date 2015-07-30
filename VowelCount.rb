0ccb077c2da6ee4d222e5f914f8f56d21f216ca4
Seung-Min Kim <kim951753@naver.com>
2015-07-30 20:01:40 +0900

    Create VowelCount.rb

---
+++ VowelCount.rb
@@ -0,0 +1,14 @@
+def VowelCount(str)
+
+  # code goes here
+  vowel = ['a','e','i','o','u']
+  ary = str.split(//)
+  counter = 0
+  ary.each do |letter|
+    if vowel.include? letter
+      counter += 1
+    end
+  end
+  return counter
+         
+end
